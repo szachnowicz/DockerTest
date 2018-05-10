e7c20cb30f6649b23af1612c69755519496c5737
Sebasitan Szachnowicz <sebastianszachnowicz@gmail.com>
2018-05-10 20:10:32 +0200

    typo

--- test/com/szachnowicz/MainTest.java
+++ test/com/szachnowicz/MainTest.java
@@ -9,25 +9,26 @@ public class MainTest {
     @Test
     public void simpleRandomTest1() {
         long l = System.currentTimeMillis();
-        assertTrue(l % 2 == 0);
+        assertTrue(l % 1 == 0);
     }
 
     @Test
     public void simpleRandomTest2() {
         long l = System.currentTimeMillis();
-        assertTrue(l % 3 == 0);
+        assertTrue(l % 2 == 0);
     }
+
     @Test
     public void simpleRandomTest3() {
         long l = System.currentTimeMillis();
         assertTrue(l % 3 == 0);
     }
+
     @Test
     public void simpleRandomTest4() {
         long l = System.currentTimeMillis();
         assertTrue(l % 4 == 0);
     }
-
 
 
 }
