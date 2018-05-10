7c1de0823adeb64c392fde23cf3249ced0f5877e
Sebasitan Szachnowicz <sebastianszachnowicz@gmail.com>
2018-05-10 20:09:58 +0200

    Initial commit

---
+++ src/com/szachnowicz/Main.java
@@ -0,0 +1,8 @@
+package com.szachnowicz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ test/com/szachnowicz/MainTest.java
@@ -0,0 +1,33 @@
+package com.szachnowicz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void simpleRandomTest1() {
+        long l = System.currentTimeMillis();
+        assertTrue(l % 2 == 0);
+    }
+
+    @Test
+    public void simpleRandomTest2() {
+        long l = System.currentTimeMillis();
+        assertTrue(l % 3 == 0);
+    }
+    @Test
+    public void simpleRandomTest3() {
+        long l = System.currentTimeMillis();
+        assertTrue(l % 3 == 0);
+    }
+    @Test
+    public void simpleRandomTest4() {
+        long l = System.currentTimeMillis();
+        assertTrue(l % 4 == 0);
+    }
+
+
+
+}
